68e036a9e4610de53989ce0c5ef6dfbada4fc7fc
samymity <samymity@gmail.com>
2025-06-13 12:22:50 -0500

    Implement initial application structure and configuration for Spring Boot project

---
+++ src/main/java/com/example/demo/utility/ChaarcterFrequencyInString.java
@@ -0,0 +1,29 @@
+package com.example.demo.utility;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class  ChaarcterFrequencyInString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("sandeep");
+		
+		
+		String s ="sandeep";
+		
+	
+		Map<Character, Long> map = s.chars()
+	            .mapToObj(c -> (char) c)
+	            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        map.forEach((character, count) -> 
+            System.out.println(character + ": " + count));
+		
+	}
+
+}
